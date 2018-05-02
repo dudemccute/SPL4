141e1ee30562382838234ebcc166ec55fa583e4f
dudemccute <171witb15@o365.htl-leoben.at>
2018-05-02 11:53:56 +0200

    Noten

---
+++ Notendurschnitt/src/Noten.java
@@ -0,0 +1,14 @@
+
+public class Noten {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] Noten = {1,1,3,2,4,5,5,4,4,3,2,2,1,1,3,2,4,5,4,3,2,1,4,4,3,2,1,1,5,3}
+		
+		
+		for(i = 0; i <Noten.length; i++) {
+			
+		}
+	}
+
+}
